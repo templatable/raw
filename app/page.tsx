12113197a11bec3f07463fc8c7cb3eee3f811f64
templatable <classprohelp@gmail.com>
2025-03-05 09:35:58 +0530

    feat: new template

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,61 @@
-import Image from "next/image";
+import Link from 'next/link'
+import { FaGithub, FaHeart } from 'react-icons/fa6'
+import { HiTemplate } from 'react-icons/hi'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+    <div className="min-h-screen min-w-screen h-full flex items-center justify-center">
+      <Link
+        href="https://github.com/templatable/raw"
+        className="text-foreground absolute top-8 right-8 p-4 border-2 rounded-full text-3xl border-border transition-all duration-150 hover:border-b-foreground"
+      >
+        <FaGithub />
+      </Link>
+      <div className="w-full flex flex-col max-w-xl mx-auto">
+        <div className="p-8 pb-6 flex flex-col border-b-2 border-border">
+          <h1 className="font-semibold text-4xl">Raw Template</h1>
+          <p className="font-medium text-md mt-2 opacity-70">
+            Get Started by editing{' '}
+            <Link
+              href=""
+              className="mx-1 font-mono p-0.5 px-1 bg-border/10 rounded-md border-border border"
+            >
               app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            </Link>{' '}
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <div className="px-12 py-6 flex flex-col">
+          <h2 className="font-semibold text-2xl">Features</h2>
+          <ul className="list-disc *:font-medium *:opacity-80 list-inside mt-4 space-y-2 px-4">
+            <li>Next.js</li>
+            <li>React</li>
+            <li>TypeScript</li>
+            <li>Tailwind CSS</li>
+            <li>React Icons</li>
+          </ul>
+        </div>
+      </div>
+      <div className="absolute bottom-20">
+        <Link
+          href="https://github.com/templatable?tab=repositories"
+          className="font-semibold opacity-80 py-2 border-b border-b-transparent hover:border-b-foreground transition-all duration-200"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <HiTemplate className="inline text-lg mr-1" /> Other Templates
+        </Link>
+      </div>
+      <div className="absolute bottom-6 flex left-0 w-screen items-center justify-center">
+        <p className="text-xs opacity-60">
+          Made with{' '}
+          <FaHeart className="text-[#FF6165] text-sm mx-0.5 mb-0.5 inline" /> by{' '}
+          <a
+            href="marban.is-a.dev"
+            className="text-foreground font-medium underline decoration-1 decoration-[#FF6165]"
+          >
+            marban
+          </a>
+        </p>
+      </div>
     </div>
-  );
+  )
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  poweredByHeader: false,
+  reactStrictMode: true,
+  compress: true,
+  productionBrowserSourceMaps: true,
+  experimental: {
+    viewTransition: true,
+    reactCompiler: true,
+    optimizeCss: true,
+    optimizeServerReact: true,
+    optimisticClientCache: true,
+    // ppr: "incremental",
+    scrollRestoration: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
